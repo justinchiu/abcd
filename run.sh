db4050c83d0edc985d0ed96cbe63cd589df0fbef
Justin Chiu <chiu.justin.t@gmail.com>
2022-11-16 23:40:50 -0500

    add requirements, run black

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 # -------- ACTION STATE TRACKING -----------
 # >>> Training <<<
-python main.py --learning-rate 1e-5 --weight-decay 0 --batch-size 3 --epochs 21 --log-interval 400 \
-    --grad-accum-steps 4 --model-type roberta --prefix 0524 --filename final --task ast
+python main.py --learning-rate 4e-5 --weight-decay 0 --batch-size 32 --epochs 21 --log-interval 400 \
+    --grad-accum-steps 1 --model-type roberta --prefix 0524 --filename final --task ast
 # >>> Examine Results <<<
 python main.py --do-eval --quantify  --model-type roberta --prefix 0524 --filename final --task ast
 
 # -------- CASCADING DIALOG SUCCESS ------------
 # Preprocess utterances if running for the first time, change model-type as needed
-# PYTHONPATH=. python utils/embed.py
+PYTHONPATH=. python utils/embed.py
 # >>> Training <<<
 python main.py --learning-rate 3e-5 --weight-decay 0 --batch-size 10 --epoch 14 --log-interval 600 \
     --model-type bert --prefix 0524 --filename final --task cds
