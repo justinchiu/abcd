6a562893e569b8b6c2d1ab53d5c3bfcfd35bfaa1
Justin Chiu <chiu.justin.t@gmail.com>
2023-02-13 00:43:35 -0500

    .

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##SBATCH -J ws-semisup                       # Job name
-#SBATCH -J oracle-sent-dummy                       # Job name
-##SBATCH -J sent256                       # Job name
+##SBATCH -J oracle-sent-dummy                       # Job name
+#SBATCH -J sent256                       # Job name
 ##SBATCH -J sent512                       # Job name
 #SBATCH -o slurm/sent_%j.out          # output file (%j expands to jobID)
 #SBATCH -e slurm/sent_%j.err          # error log file (%j expands to jobID)
@@ -13,7 +13,7 @@
 #SBATCH --mem=32000                          # server memory requested (per node)
 #SBATCH -t 48:00:00                           # Time limit (hh:mm:ss)
 ##SBATCH --nodelist=rush-compute-02 # Request partition
-##SBATCH --nodelist=rush-compute-01 # Request partition
+#SBATCH --nodelist=rush-compute-01 # Request partition
 #SBATCH --partition=rush,gpu # Request partition
 #SBATCH --gres=gpu:1                  # Type/number of GPUs needed
 ##SBATCH --gres=gpu:a6000:1                  # Type/number of GPUs needed
@@ -60,4 +60,8 @@ py113env
 #python run_oracle_sent_model.py --batch_size 4 --eval_batch_size 4 --max_length 256 --eval_steps 250 --epoch 10 --prefix 26 --gradient_accumulation_steps 4 --dummy_step
 #python run_oracle_sent_model.py --batch_size 2 --eval_batch_size 4 --max_length 512 --eval_steps 250 --epoch 10 --prefix 26 --gradient_accumulation_steps 8 --dummy_step
 # oracle sent init from previous rerun
-python run_oracle_sent_model.py --batch_size 2 --eval_batch_size 4 --max_length 512 --eval_steps 250 --epoch 10 --prefix 26 --gradient_accumulation_steps 8 --init_from_previous
+#python run_oracle_sent_model.py --batch_size 2 --eval_batch_size 4 --max_length 512 --eval_steps 250 --epoch 10 --prefix 26 --gradient_accumulation_steps 8 --init_from_previous
+
+# oracle sent bart-large ml256 2/13
+#python run_oracle_sent_model.py --batch_size 4 --eval_batch_size 4 --max_length 256 --eval_steps 250 --epoch 10 --prefix 213 --gradient_accumulation_steps 4 --init_from_previous --monotonic_train --max_turns 128 --answer_model_dir facebook/bart-large
+python run_oracle_sent_model.py --batch_size 4 --eval_batch_size 4 --max_length 256 --eval_steps 250 --epoch 10 --prefix 213 --gradient_accumulation_steps 4 --init_from_previous --monotonic_train --max_turns 128 --answer_model_dir facebook/bart-large --learning_rate 1e-5
